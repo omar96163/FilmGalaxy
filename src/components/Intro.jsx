56b046f1214bc2faa59663514107a0e0322a46d8
Omar Albaz <omaralbaz635@gmail.com>
2025-02-16 15:36:37 +0200

    Add new file : .gitattributes

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -11,7 +11,7 @@ function Intro() {
     <>
       <div className="relative w-full h-screen flex items-center justify-center text-white">
         <video
-          src="/videos/Wolf Man.mp4"
+          src="/videos/intro.mp4"
           autoPlay
           loop
           muted
