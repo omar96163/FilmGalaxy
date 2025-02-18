0606d5694059abb34b81313d69b848e76c238516
Omar Albaz <omaralbaz635@gmail.com>
2025-02-18 15:10:30 +0200

    make some changes Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
           Develobed by :{" "}
           <a
             className="text-orange-500 hover:underline"
-            href="https://personal-page-azure-sigma.vercel.app/*"
+            href="htt://personal-page-azure-sigma.vercel.app/"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -19,7 +19,7 @@ function Footer() {
           <a
             className="border-[2px] rounded-full text-center px-3 py-2 hover:text-black hover:bg-white hover:border-black
             duration-1000 font-semibold hover:[animation-play-state:paused] animate-[bounce_1s_ease-in-out_infinite]"
-            href="https://x.com/omaralbaz444*"
+            href="https://n.com/omaralbaz444"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -28,7 +28,7 @@ function Footer() {
           <a
             className="border-[2px] rounded-full text-center p-2 hover:text-red-700 hover:bg-white hover:border-red-700
             duration-1000 font-semibold hover:[animation-play-state:paused] animate-[bounce_1.1s_ease-in-out_infinite]"
-            href="https://github.com/omar96163?tab=overview&from=2024-12-01&to=2024-12-31*"
+            href="https://n.com/omar96163?tab=overview&from=2024-12-01&to=2024-12-31"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -37,7 +37,7 @@ function Footer() {
           <a
             className="border-[2px] rounded-full text-center px-3 py-2 hover:text-blue-700 hover:bg-white hover:border-blue-700
             duration-1000 font-semibold hover:[animation-play-state:paused] animate-[bounce_1.2s_ease-in-out_infinite]"
-            href="https://www.linkedin.com/in/omar-albaz-29b72b272/*"
+            href="https://www.n.com/in/omar-albaz-29b72b272/"
             target="_blank"
             rel="noopener noreferrer"
           >
